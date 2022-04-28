c8ace9e1c6b3499bb973aa4e6c72afd59b1ecb93
Maxim <swiftdevfromsiberia@gmail.com>
2022-04-28 22:40:34 +0500

    Initial Commit

---
+++ ToDoFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoFirebase
+//
+//  Created by Makarov_Maxim on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
