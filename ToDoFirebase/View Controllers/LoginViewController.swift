aa3b360ccb0329076e6336a46b1ad1b58cd8af55
Maxim <swiftdevfromsiberia@gmail.com>
2022-05-01 22:30:12 +0500

    All views are set, buttons and outlets are added

---
+++ ToDoFirebase/View Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ToDoFirebase
+//
+//  Created by Makarov_Maxim on 28.04.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+}
+
